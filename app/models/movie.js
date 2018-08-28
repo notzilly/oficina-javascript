cc9a02fe5396db4b2b27a62e5245a3c914fcd523
Luis Henrique Medeiros <lmedeiros@inf.ufsm.br>
2018-08-27 23:02:26 -0300

    Movies now have array of Strings to represent their genres

--- app/models/movie.js
+++ app/models/movie.js
@@ -2,14 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var MovieSchema = new Schema({
-    id: Number,
     primaryTitle: String,
     originalTitle: String,
     releaseYear: Number,
     runtimeMinutes: Number,
-    genres: [{
-        name: String
-    }]
+    genres: [String]
 });
 
 module.exports = mongoose.model('Movie', MovieSchema);
