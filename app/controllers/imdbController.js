9bdc382239447756ae5b63b25a85d860c39c1b70
Luis Henrique Medeiros <lmedeiros@inf.ufsm.br>
2018-08-29 12:00:17 -0300

    Movie post and get routes added

--- app/controllers/imdbController.js
+++ app/controllers/imdbController.js
@@ -10,6 +10,40 @@ router.get('/filmes', function(req, res) { // get all the movies
         res.json({ message: movies });
     });
 
+}).get('/filmes/:filme_id', function(req, res) { // get a specific movie by id
+
+    Imdb.findOne({ _id: req.params.filme_id, titleType: 'movie' }, function(err, entry) {
+        if(err) res.json({ message: 'Erro ao buscar por filme' });
+        res.json({ message: entry });
+    });
+
+}).post('/filmes', function(req, res) { // create a new movie
+
+    console.log(req.body);
+
+    var movie = new Imdb();
+    movie.titleType = 'movie';
+    movie.primaryTitle = req.body.tituloPrimario;
+    movie.originalTitle = req.body.tituloOriginal;
+    movie.startYear = req.body.anoInicio;
+    movie.endYear = req.body.anoFim;
+    movie.runtimeMinutes = req.body.duracaoMinutos;
+    movie.genres = req.body.generos;
+
+    console.log(movie);
+
+    // movie.save(err => {
+    //     if(err) return res.status(500).send(err);
+    //     return res.status(200).send(movie);
+    // });
+
+// }).delete('/filmes/:filme_id', function(req, res) { // deletes a movie
+
+//     Imdb.findOne({ _id: req.params.filme_id, titleType: 'movie' }, function(err, entry) {
+//         if(err) res.json({ message: 'Erro ao buscar por entrada específica' });
+//         res.json({ message: entry });
+//     });
+
 }).get('/series', function(req, res) { // get all the tvseries
 
     Imdb.find({ titleType: 'tvSeries' }, function(err, series) {
@@ -22,13 +56,6 @@ router.get('/filmes', function(req, res) { // get all the movies
     Imdb.find({ titleType: 'short' }, function(err, shorts) {
         if(err) res.json({ message: 'Erro ao buscar todos os curtas' });
         res.json({ message: shorts });
-    });
-
-}).get('/todos/:id', function(req, res) { // get a specific entry by id
-
-    Imdb.findById(req.params.id, function(err, entry) {
-        if(err) res.json({ message: 'Erro ao buscar por entrada específica' });
-        res.json({ message: entry });
     });
 
 }).get('/normalizar', function(req, res) { // route to normalize genres of all entries
